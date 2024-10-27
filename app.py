928ab26560ea6a8ada2ef8bb43f7dc323f179f93
VovaZubiak <sashles2009@gmail.com>
2024-10-27 19:55:43 +0200

    Creation routing and templates

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, world!"
+    return render_template('resume.html', section="about")
+
+@app.route('/about')
+def about():
+    return render_template('resume.html', section="about")
+
+@app.route('/education')
+def education():
+    return render_template('resume.html', section="education")
+
+@app.route('/skills')
+def skills():
+    return render_template('resume.html', section="skills")
+
+@app.route('/experience')
+def experience():
+    return render_template('resume.html', section="experience")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
